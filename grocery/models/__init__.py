27fb3e2eaa7fd57968d7fc3cfe22f069dbae6e8c
Om Hursal <op83846@gmail.com>
2026-03-07 23:58:33 +0530

    Updated Review model

---
+++ grocery/models/__init__.py
@@ -0,0 +1,10 @@
+from .category import Category, SubCategory
+from .product import Product
+from .cart import Cart, CartItem
+from .wishlist import Wishlist
+from .coupon import Coupon
+from .address import Address
+from .delivery import DeliverySlot
+from .order import Order, OrderItem
+from .profile import Profile
+from .review import Review
